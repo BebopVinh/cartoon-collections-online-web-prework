6840a35afeefcdb8e9c3b9ad526d973f92fa51c5
Vinh Do <vinhdo07@gmail.com>
2019-01-17 03:07:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ end
 
 def summon_captain_planet(array)
   array_2 = []
-  array.each do |element|
-    array_2 << element
+  array.each do |word|
+    array_2 << "#{word.capitalize}!"
   end
   return array_2
 end
